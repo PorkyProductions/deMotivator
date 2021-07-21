931032d0915b9fb7c94199e3877bc22b58027e13
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2021-07-21 12:02:43 -0400

    Added a java test and updated the changelog.

---
+++ Coffee Test.java
@@ -0,0 +1,6 @@
+public class myfirstclass{
+    public static void main(String[] args)
+    {
+                System.out.println(“If you can see this, that means that demotivator is ready to use. Enjoy!”);
+    }
+    }
