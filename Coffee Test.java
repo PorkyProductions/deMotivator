ae4dd128d905209aa627fe805eed3c7f1b064770
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2021-11-18 10:11:48 -0500

    Clean up the clutter

--- Coffee Test.java
+++
@@ -1,6 +0,0 @@
-public class myfirstclass{
-    public static void main(String[] args)
-    {
-                System.out.println(“If you can see this, that means that demotivator is ready to use. Enjoy!”);
-    }
-    }
