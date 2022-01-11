67aa1dc6bacae7c91bf5e5580c3d9a653fd43c2d
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-01-11 12:24:52 -0500

    began the refactor of the entire ts lib

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let insults = 
-[
+var insults = [
     "you are dog water",
     "you bad",
     "you have the same chance of reproducing as a computer mouse",
@@ -164,7 +163,7 @@ let insults =
     "You have a room temperature IQ - if the room is in Antarctica.",
     "Do you want to know how I get all these insults? I use something called intelligence.",
     "You are so poor when you saw the garbage truck pass you took a shopping list with.",
-    function() {getSelection();},
+    function () { getSelection(); },
     "You're so ugly, when you got robbed, the robbers made you wear their masks to look like your clothes.",
     "You're so ugly, when you went to the DMV, they had to roll out your windows so that other drivers could see your face.",
     "You're so ugly, when you went to the DMV, they didn't let you in because you were in the front seat.",
@@ -193,14 +192,11 @@ let insults =
     "You're so fat, you could sell shade.",
     "You're so ugly, when you got robbed, the robbers made you wear their masks to look like your clothes.",
     "You're so ugly, when you went to the therapist, he gave you injections for your face.",
-
-    
-
-    
 ];
 
-function GenerateInsult(){
-    insult = insults[Math.floor(Math.random() * (insults.length-0.01))];
+
+function GenerateInsult() {
+    insult = insults[Math.floor(Math.random() * (insults.length - 0.01))];
     console.log(insults);
     document.getElementById("insult").innerHTML = insult;
-  }
+}
