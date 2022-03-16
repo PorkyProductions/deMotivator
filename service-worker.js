599c30a54ee74bb606ae4b4db676442809b42f9a
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-03-16 17:58:28 -0400

    UGHHHHHHHHHHH

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,9 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
-workbox.router.registerRoute(({ request }) => request.destination === 'image', new workbox.strategies.NetworkFirst());
+importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+  );
+
+workbox.router.registerRoute(
+  ({request}) => request.destination === 'image',
+  new workbox.strategies.NetworkFirst()
+
+);
