abefc7b20bcd313de4fb996821fbe5e0368daa1a
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-03-17 12:24:22 -0400

    made the counter better

--- ts/service-worker.ts
+++ ts/service-worker.ts
@@ -2,7 +2,7 @@ importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
   );
 
-workbox.router.registerRoute(
+workbox.routing.registerRoute(
   ({request}) => request.destination === 'image',
   new workbox.strategies.NetworkFirst()
 
