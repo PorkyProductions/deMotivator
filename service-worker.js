e9836c37509572ef4e6749bf301f11bc281e1209
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-03-25 16:47:54 -0400

    revert: :rewind: goback to orig service worker

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,9 @@
-const CACHE = "pwabuilder-offline";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
 
 workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
+({request}) => request.destination === 'image',
+new workbox.strategies.CacheFirst()
+
 );
-// hello
