09000f4ea68f524f5b07253ae6573ddb63c8c475
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-03-25 16:36:43 -0400

    revert: :rewind: go back before it broke

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,17 @@
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-  );
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
 
 workbox.routing.registerRoute(
-  ({request}) => request.destination === 'image',
-  new workbox.strategies.CacheFirst()
-
+  new RegExp('/deMotivator'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
 );
+// hello
