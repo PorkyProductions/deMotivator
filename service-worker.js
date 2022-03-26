4c260ae8d3a29ff3b5b9ca58bede0e67e736faea
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-03-25 22:55:11 -0400

    feat: :zap: add a offline mode

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,18 @@
+const CACHE = "pwabuilder-offline";
+
 importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
-workbox.routing.registerRoute(
-({request}) => request.destination === 'image',
-new workbox.strategies.CacheFirst()
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
 
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
 );
-// hi
