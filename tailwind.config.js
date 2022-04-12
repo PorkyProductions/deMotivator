0952bc6888b12dd5ba6814eb5730c2093b02c570
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-04-12 16:20:12 -0400

    feat: :sparkles: Redid the API page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["index.html", "about.html", "credits.html", "api.html"],
+  content: ["index.html", "about.html", "credits.html", "api.html", "404.html",],
   theme: {
     extend: {},
   },
