e404cd049f6d729180daa59d7fd49ca1541af94b
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-05-25 09:58:26 -0400

    worked on 404 page

---
+++ js/404.js
@@ -0,0 +1,4 @@
+const noHog = document.getElementById('noHog');
+noHog.addEventListener('mouseover', function () {
+    noHog.classList.add('spin');
+});
---
+++ ts/404.ts
@@ -0,0 +1,6 @@
+const noHog: HTMLElement = document.getElementById('noHog');
+
+
+noHog.addEventListener('mouseover', () => {
+    noHog.classList.add('spin');
+});
