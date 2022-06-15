eca612f2c079ec528bf9c4e82598eb1f2cb3e399
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-06-15 14:35:59 -0400

    chore: :tada: scaffold out basic project

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    rollupOptions: {
+        input: {
+            index: 'index.html',
+            fourOhFour: '404.html',
+        },
+        output: {
+            dir: 'www',
+        }
+    }
+  }
+})
