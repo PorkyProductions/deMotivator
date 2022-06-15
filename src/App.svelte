b7f476d831ff0467196cf92b38e1ea3efcfa7a71
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-06-15 15:01:13 -0400

    feat: :sparkles: added title

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,13 @@
 <script>
-
+    import './app.css'
+import Title from './components/title.svelte';
 </script>
 
 <main>
-  
+    <div class="dark:bg-black dark:text-white" id="app">
+        <Title />
+    </div>
+    <div id="footer">
+
+    </div>
 </main>
---
+++ src/components/title.svelte
@@ -0,0 +1,5 @@
+<main>
+    <h1 class="font-primary text-center text-4xl p-5 font-semibold">
+        (de)Motivator
+    </h1>
+</main>
