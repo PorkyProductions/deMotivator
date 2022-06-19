a09febc46d5b139b7d77b2a49144eb0f7ab7545d
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-06-18 22:17:50 -0400

    feat: :rocket: getting ready for final deploy

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
     rollupOptions: {
         input: {
             index: 'index.html',
-            fourOhFour: './src/pages/404/404.html',
-            fiveHundred: './src/pages/500/500.html',
-            // DMV1: './src/pages/dmv1.html',
+            fourOhFour: './404.html',
+            fiveHundred: './500.html',
+            // DMV1: './dmv1.html',
         },
         output: {
             dir: 'docs',
