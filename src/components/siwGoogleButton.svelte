8df7296548a17bca68982ccbfa9c2a1cea51d172
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-07-14 16:33:51 -0400

    Update siwGoogleButton.svelte

--- src/components/siwGoogleButton.svelte
+++ src/components/siwGoogleButton.svelte
@@ -37,7 +37,7 @@ const loginFunction = () => {
         const token = credential.accessToken;
         // The signed-in user info.
         const user = result.user;
-        loggedIn = true
+        loggedIn = true;
     }).catch((error) => {
         // Handle Errors here.
         const errorCode = error.code;
@@ -48,11 +48,30 @@ const loginFunction = () => {
         const credential = GoogleAuthProvider.credentialFromError(error);
         // ...
     });
+};
+
+
+// This following block of code gets the basic metadata about the user
+
+const user = auth.currentUser;
+if (user !== null) {
+    // The user object has basic properties such as display name, email, etc.
+    const displayName = user.displayName;
+    const email = user.email;
+    const photoURL = user.photoURL;
+    const emailVerified = user.emailVerified;
+
+    // The user's ID, unique to the Firebase project. Do NOT use
+    // this value to authenticate with your backend server, if
+    // you have one. Use User.getToken() instead.
+    const uid = user.uid;
 }
 
+// The logout function
+// TODO: Make it functionally callable 
+
 const logoutFunction = (auth) => {
-    signOut(auth)
-    .then(() => {
+    signOut(auth).then(() => {
         loggedIn = false
     }).catch((error) => {
         alert(`
@@ -61,21 +80,25 @@ const logoutFunction = (auth) => {
             ${error}
         `)
     });
-}
-
+};
 
 </script>
 
 
 {#if loggedIn}
     <button on:click={logoutFunction} class="flex justify-between content-center bg-white text-black p-4 rounded-lg">
-        <p>Sign Out</p>
+        {#if user.isAnonymous}
+            <p>Sign Out</p>
+        {:else if !user.isAnonymous}
+            <p>Sign Out {user.displayName}</p>
+        {:else}
+            <p>Sign Out</p>
+        {/if}
     </button>
 {:else if !loggedIn}
     <button on:click={loginFunction} class="flex justify-between content-center bg-white text-black p-4 rounded-lg">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
+        <p>Sign In with</p> <br> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
             <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
         </svg>
-        <p>Sign In with Google</p>
     </button>
 {/if}
