4d2b673e2a9eb0352cfa86ba5979dda3402bb48f
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-11-21 11:39:53 -0500

    refactor(rm): :fire: remove generated files

--- electron/app/assets/random.170635ff.js
+++
@@ -1,1 +0,0 @@
-const o=(a,n)=>Math.round(Math.random()*(n-a)+a);export{o as randomInRange};
--- electron/app/assets/web.5b657ccf.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index.4a1ce933.js";class r extends n{async alert(e){window.alert(e.message)}async prompt(e){const a=window.prompt(e.message,e.inputText||"");return{value:a!==null?a:"",cancelled:a===null}}async confirm(e){return{value:window.confirm(e.message)}}}export{r as DialogWeb};
