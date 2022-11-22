af74db3f791c06ef68e1911a17ba1610022c3ea7
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-11-22 17:23:28 -0500

    revert: :rewind: jk we dont support legacy brosers
    yet

--- vite.config.ts
+++ vite.config.ts
@@ -7,24 +7,26 @@ import { UserConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    svelte(),
-    legacy()
-  ],
+      svelte(),
+      // legacy({ 
+      //   targets: [">0.3%, cover 70%, not dead"] 
+      // })
+    ],
   base: "/",
   build: {
     rollupOptions: {
       input: {
-        index: 'index.html',
-        fourOhFour: './404.html',
-        fiveHundred: './500.html',
-        login: './login.html',
-        signUp: './signUp.html',
-        list: './list.html'
+        index: "index.html",
+        fourOhFour: "./404.html",
+        fiveHundred: "./500.html",
+        login: "./login.html",
+        signUp: "./signUp.html",
+        list: "./list.html",
         // DMV1: './dmv1.html',
       },
       output: {
-        dir: 'out',
-      }
-    }
-  }
-})
+        dir: "out",
+      },
+    },
+  },
+});
