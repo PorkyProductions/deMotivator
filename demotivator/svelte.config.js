62517a82ba5db1601e8cbaf06b1bfc23dc1e8826
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2023-02-03 16:00:37 -0500

    refactor: :construction: switch from SSR to SSG

--- demotivator/svelte.config.js
+++ demotivator/svelte.config.js
@@ -1,11 +1,19 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({
+		// default options are shown. On some platforms
+		// these options are set automatically — see below
+		pages: 'build',
+		assets: 'build',
+		fallback: null,
+		precompress: false,
+		strict: true
+	}),
 
 	kit: {
 		adapter: adapter()
@@ -13,3 +21,12 @@ const config = {
 };
 
 export default config;
+// preprocess: vitePreprocess({
+// 	// default options are shown. On some platforms
+// 	// these options are set automatically — see below
+// 	pages: 'build',
+// 	assets: 'build',
+// 	fallback: null,
+// 	precompress: false,
+// 	strict: true
+// }),
