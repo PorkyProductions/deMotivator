f2a1aefbb4b6d7b42e628bd02bb42a3b62cdf527
Ryan Mullin <82683251+hiteacheryouare@users.noreply.github.com>
2025-11-15 00:10:08 -0500

    refactor: update package.json and improve app initialization across multiple pages

---
+++ src/types/shims-vendor.d.ts
@@ -0,0 +1,6 @@
+declare module 'uadetect/getCurrentUA';
+declare module 'uadetect/deviceType';
+declare module '@porkyproductions/hat/wait';
+declare module '@porkyproductions/hat/randomInRange';
+declare module '@porkyproductions/hat/randomInArray';
+declare module '@porkyproductions/hat/print';
