b1138783f358bf2a34a6692701eb6f071b8d4e91
Ryan Mullin <82683251+hiteacheryouare@users.noreply.github.com>
2025-11-23 14:27:17 -0500

    get rid of older pages

--- src/pages/share/twitter.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    import { tweets } from './tweets'
-
-</script>
-<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text={tweets[Math.floor(Math.random() * tweets.length)]} data-hashtags="ouch" data-show-count="false">Share on Twitter</a>
