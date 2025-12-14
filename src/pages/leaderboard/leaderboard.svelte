6a4127400643ebe60c79f2d859d3fda6ad020193
Ryan Mullin <82683251+hiteacheryouare@users.noreply.github.com>
2025-12-14 17:39:19 -0500

    refactor: clean up leaderboard page layout by removing unnecessary back to home button

--- src/pages/leaderboard/leaderboard.svelte
+++ src/pages/leaderboard/leaderboard.svelte
@@ -6,9 +6,3 @@
 
 <LeaderboardMain />
 <Footer />
-
-<div class="container text-center mt-5 pb-5">
-	<a href="/" class="btn btn-outline-secondary btn-lg">
-		<Icon name="house-door" /> Back to Home
-	</a>
-</div>
