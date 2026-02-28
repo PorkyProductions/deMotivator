3a298daa5e82341fca06226916e342457bc356ea
Ryan Mullin <82683251+hiteacheryouare@users.noreply.github.com>
2026-02-27 23:34:08 -0500

    fix(navigation): update GitHub link for (de)Motivator.js

--- apps/web/src/utils/navigation.ts
+++ apps/web/src/utils/navigation.ts
@@ -9,7 +9,7 @@ export interface NavItem {
 export const navigation: NavItem[] = [
 	{ name: 'Home', shortName: 'Home', href: '/', icon: 'house-door-fill' },
 	{ name: '(de)Motivator 1.0', shortName: 'v1.0', href: 'dmv1.html', icon: 'clock-history' },
-	{ name: '(de)Motivator.js', shortName: '.js', href: 'https://github.com/PorkyProductions/deMotivator.js', icon: 'github', external: true },
+	{ name: '(de)Motivator.js', shortName: '.js', href: 'https://github.com/PorkyProductions/deMotivator', icon: 'github', external: true },
 	{ name: 'Settings', shortName: 'Settings', href: 'settings.html', icon: 'gear-fill' },
 	{ name: 'Leaderboard', shortName: 'Top', href: 'leaderboard.html', icon: 'trophy-fill' }
 ];
