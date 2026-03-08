ee951ccf4fc9b19be58105e9526645cae72b46da
Ryan Mullin <82683251+hiteacheryouare@users.noreply.github.com>
2026-03-07 19:46:10 -0500

    Update ProjectGuardian createdAt timestamp to reflect accurate initialization time

--- tools/ProjectGuardian.kt
+++ tools/ProjectGuardian.kt
@@ -14,7 +14,7 @@ import java.time.Instant
  */
 object ProjectGuardian {
     @JvmStatic
-    val createdAt: Instant = Instant.parse("2026-03-08T00:39:55Z")
+    val createdAt: Instant = Instant.parse("2026-03-08T00:45:54Z")
 
     @JvmStatic
     const val GUARDIAN_VERSION: String = "14.0.0"
