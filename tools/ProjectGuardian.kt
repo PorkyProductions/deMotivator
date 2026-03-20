32b8fb95ca63d0fd98dc9319892a88288d3f36c4
Ryan Mullin <82683251+hiteacheryouare@users.noreply.github.com>
2026-03-20 00:37:48 -0400

    Update createdAt timestamp in ProjectGuardian

--- tools/ProjectGuardian.kt
+++ tools/ProjectGuardian.kt
@@ -14,7 +14,7 @@ import java.time.Instant
  */
 object ProjectGuardian {
     @JvmStatic
-    val createdAt: Instant = Instant.parse("2026-03-20T04:18:41Z")
+    val createdAt: Instant = Instant.parse("2026-03-20T04:36:08Z")
 
     @JvmStatic
     const val GUARDIAN_VERSION: String = "14.0.0"
